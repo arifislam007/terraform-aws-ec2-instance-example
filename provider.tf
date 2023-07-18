4cfa5445cd23dc15e33280c43234012786333641
MD. AREFUL ISLAM <32135229+arifislam007@users.noreply.github.com>
2023-07-18 22:22:04 +0600

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "XXXXXXXXXX" # just write your access key here 
+  secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX" # Write your secret key here
+  region = "us-east-1" # You can select your region from here.
+}
